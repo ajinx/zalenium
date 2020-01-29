3ecdaeb59ddb99373339150de9bbd50408fc23c3
ajinx <jinkiestheone@gmail.com>
2020-01-29 12:20:26 -0500

    Fixing the bug for self hosted runner
    Stopping and starting zalenium to avoid the issue

--- index.js
+++ index.js
@@ -1,5 +1,8 @@
 const { execSync } = require("child_process");
 
+console.log(`## STOPPING ZALENIUM`);
+execSync(`curl -sSL https://raw.githubusercontent.com/dosel/t/i/p | bash -s stop`)
+
 console.log(`## STARTING ZALENIUM`);
 
 execSync(`curl -sSL https://raw.githubusercontent.com/dosel/t/i/p | bash -s start`)
